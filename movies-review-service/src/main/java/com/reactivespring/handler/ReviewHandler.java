74fe03ebc2d552c864e5b817e6cc767c768c17a2
IvanKrstic123 <krstic8687@yahoo.com>
2022-02-08 10:36:48 +0100

    added Router Function and handler for POST request for creating Review

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -1,4 +1,31 @@
 package com.reactivespring.handler;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
 public class ReviewHandler {
+
+    private ReviewReactiveRepository reviewReactiveRepository;
+
+    public ReviewHandler(ReviewReactiveRepository reviewReactiveRepository) {
+        this.reviewReactiveRepository = reviewReactiveRepository;
+    }
+
+    // persisting data in database and building ServerResponse
+    public Mono<ServerResponse> addReview(ServerRequest request) {
+        return request.bodyToMono(Review.class)
+                .flatMap(review -> {
+                    return reviewReactiveRepository.save(review);
+                })
+                .flatMap(savedReview -> {
+                    return ServerResponse.status(HttpStatus.CREATED)
+                            .bodyValue(savedReview);
+                });
+    }
 }
