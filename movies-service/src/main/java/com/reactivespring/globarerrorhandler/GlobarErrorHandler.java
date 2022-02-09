a89f6cb2c740dbdc60a6180624abb6dc3f7ddc08
IvanKrstic123 <krstic8687@yahoo.com>
2022-02-09 19:26:25 +0100

    added handling 5xx in MoviesInfoService in WebClient

--- movies-service/src/main/java/com/reactivespring/globarerrorhandler/GlobarErrorHandler.java
+++ movies-service/src/main/java/com/reactivespring/globarerrorhandler/GlobarErrorHandler.java
@@ -1,7 +1,9 @@
 package com.reactivespring.globarerrorhandler;
 
 import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,5 +16,11 @@ public class GlobarErrorHandler {
     public ResponseEntity<String> handleClientException(MoviesInfoClientException exception)  {
         log.error("Exception Caught in handleClientException : {}", exception.getMessage());
         return ResponseEntity.status(exception.getStatusCode()).body(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleServerException(RuntimeException exception)  {
+        log.error("Exception Caught in handleClientException : {}", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 }
